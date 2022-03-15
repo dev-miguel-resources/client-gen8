535446916b564dbb63021f7701c873e30cc54b7d
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-03-15 20:29:28 -0300

    code splitting and routes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,14 @@
 import { combineReducers } from "redux";
 import { userReducer } from "./userReducer";
 import { headerReducer } from "./headerReducer";
+import { searchReducer } from "./searchReducer";
+import { cartReducer } from "./cartReducer";
 
 const rootReducer = combineReducers({
   user: userReducer,
   header: headerReducer,
+  search: searchReducer,
+  cart: cartReducer,
 });
 
 export default rootReducer;
